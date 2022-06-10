852ef1528d3e4215b18c05c679beb2020fa74906
rehan-sattar <rehansattar117@gmail.com>
2022-06-10 10:08:13 +0500

    New: Add environment configs, typeorm config and database connection

---
+++ server/src/main.ts
@@ -0,0 +1,22 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get<number>('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(port, () => {
+    Logger.log(
+      `Server Running At: ${config.get<string>('BASE_URL')}`,
+      'NestApplication',
+    );
+  });
+}
+bootstrap();
