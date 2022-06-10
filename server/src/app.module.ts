ff68411bfc81ab0011f36badba77f6eb6882a8e4
rehan-sattar <rehansattar117@gmail.com>
2022-06-10 14:30:49 +0500

    Improve: remove the unused files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
@@ -19,7 +17,5 @@ const envFilePath = getEnvPath(`${__dirname}/common/envs`);
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
