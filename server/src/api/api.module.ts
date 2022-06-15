ee7401910b241017edc8a055ba74e3ff340e88dc
rehan-sattar <rehansattar117@gmail.com>
2022-06-15 10:12:20 +0500

    👌 IMPROVE: Add all apis into the api module

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
 })
 export class ApiModule {}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,8 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
-import { AuthModule } from './api/auth/auth.module';
-import { UsersModule } from './api/users/users.module';
 
 const envFilePath = getEnvPath(`${__dirname}/common/envs`);
 
@@ -14,8 +12,6 @@ const envFilePath = getEnvPath(`${__dirname}/common/envs`);
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     ApiModule,
-    AuthModule,
-    UsersModule,
   ],
 })
 export class AppModule {}
